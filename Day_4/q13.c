7bda50d5ac794a5f94290c7e630b77df43fd0d28
Dishant Tomar <tomardishant08@gmail.com>
2026-06-05 23:20:01 +0530

    Add program to generate Fibonacci series

---
+++ Day_4/q13.c
@@ -0,0 +1,21 @@
+// Program to generate Fibonacci series
+
+#include <stdio.h>
+int main() {
+    int n, first = 0, second = 1, next, i;
+    // Taking input from user
+    printf("Enter number of terms: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+    // Printing Fibonacci series
+    for(i = 1; i <= n; i++)
+    {
+        printf("%d ", first);
+
+        next = first + second;
+        first = second;
+        second = next;
+    }
+    return 0;
+}
