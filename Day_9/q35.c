1f116f86d5fb358d40c31ea1541b00ed11cf9d7b
Dishant Tomar <tomardishant08@gmail.com>
2026-06-09 21:05:28 +0530

    Add program to print repeated character pattern
    This program prints a repeated character pattern based on user input for the number of rows.

---
+++ Day_9/q35.c
@@ -0,0 +1,19 @@
+// Program to print repeated character pattern
+
+#include <stdio.h>
+int main() {
+    int i, j, rows;
+    // Taking input from user
+    printf("Enter number of rows: ");
+    scanf("%d", &rows);
+    // Printing repeated character pattern
+    for(i = 1; i <= rows; i++)
+    {
+        for(j = 1; j <= i; j++)
+        {
+            printf("%c", 64 + i);
+        }
+        printf("\n");
+    }
+    return 0;
+}
