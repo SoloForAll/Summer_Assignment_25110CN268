2e2ce75be747ad795d8e43a0aacfa7ca8a8c944b
Dishant Tomar <tomardishant08@gmail.com>
2026-06-24 20:08:57 +0530

    Implement student details storage using structures
    This program allows for the input and display of multiple student details using an array of structures.

---
+++ Day_23/array_of_structures.c
@@ -0,0 +1,44 @@
+// Program to store details of multiple students using array of structures
+
+#include <stdio.h>
+
+struct Student
+{
+    int rollNo;
+    char name[50];
+    float marks;
+};
+
+int main()
+{
+    struct Student s[5];
+    int i;
+
+    // Taking details of students
+    for(i = 0; i < 5; i++)
+    {
+        printf("\nEnter details of Student %d\n", i + 1);
+
+        printf("Roll Number: ");
+        scanf("%d", &s[i].rollNo);
+
+        printf("Name: ");
+        scanf("%s", s[i].name);
+
+        printf("Marks: ");
+        scanf("%f", &s[i].marks);
+    }
+
+    // Displaying details
+    printf("\nStudent Records:\n");
+
+    for(i = 0; i < 5; i++)
+    {
+        printf("\nStudent %d\n", i + 1);
+        printf("Roll Number = %d\n", s[i].rollNo);
+        printf("Name = %s\n", s[i].name);
+        printf("Marks = %.2f\n", s[i].marks);
+    }
+
+    return 0;
+}
