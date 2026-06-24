e242b06e98a8ed3d7b99cd0554be23a05c6a0e60
Dishant Tomar <tomardishant08@gmail.com>
2026-06-24 20:09:19 +0530

    Add nested structure example for student details
    This program demonstrates the use of nested structures in C by defining a Student structure that includes an Address structure.

---
+++ Day_23/nested_structure.c
@@ -0,0 +1,43 @@
+// Program to demonstrate nested structure
+
+#include <stdio.h>
+
+struct Address
+{
+    char city[30];
+    char state[30];
+};
+
+struct Student
+{
+    int rollNo;
+    char name[50];
+    struct Address address;
+};
+
+int main()
+{
+    struct Student s;
+
+    // Taking student details
+    printf("Enter Roll Number: ");
+    scanf("%d", &s.rollNo);
+
+    printf("Enter Name: ");
+    scanf("%s", s.name);
+
+    printf("Enter City: ");
+    scanf("%s", s.address.city);
+
+    printf("Enter State: ");
+    scanf("%s", s.address.state);
+
+    // Displaying details
+    printf("\nStudent Details:\n");
+    printf("Roll Number = %d\n", s.rollNo);
+    printf("Name = %s\n", s.name);
+    printf("City = %s\n", s.address.city);
+    printf("State = %s\n", s.address.state);
+
+    return 0;
+}
