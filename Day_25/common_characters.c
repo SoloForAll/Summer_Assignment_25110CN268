f1378c1a871c62315d70c0ab5adb34dbb9810a24
Dishant Tomar <tomardishant08@gmail.com>
2026-06-26 17:50:42 +0530

    Add common characters finder program
    Implement a program to find common characters between two strings.

---
+++ Day_25/common_characters.c
@@ -0,0 +1,33 @@
+// Program to find common characters in two strings
+
+#include <stdio.h>
+
+int main()
+{
+    char str1[100], str2[100];
+    int i, j;
+
+    // Taking string input
+    printf("Enter first string: ");
+    scanf("%s", str1);
+
+    printf("Enter second string: ");
+    scanf("%s", str2);
+
+    printf("Common Characters:\n");
+
+    // Finding common characters
+    for(i = 0; str1[i] != '\0'; i++)
+    {
+        for(j = 0; str2[j] != '\0'; j++)
+        {
+            if(str1[i] == str2[j])
+            {
+                printf("%c ", str1[i]);
+                break;
+            }
+        }
+    }
+
+    return 0;
+}
